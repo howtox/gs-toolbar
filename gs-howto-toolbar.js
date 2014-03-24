c3739fecd1583ec7b1cae305618a569e7e16ce04
Shaohua Zhou <sh.zhou@gmail.com>
2014-03-23 17:52:04 -0700

    Init commit

---
+++ gs-howto-toolbar.js
@@ -0,0 +1,38 @@
+/*global document, HTMLElement */
+(function(document, $){
+
+  var doc = (document._currentScript || document.currentScript).ownerDocument;
+  var template = doc.querySelector('template').innerHTML;
+
+  var GsHowtoToolbarProto = Object.create(HTMLElement.prototype);
+
+  GsHowtoToolbarProto.createdCallback = function(){
+    console.log('createdCallback');
+    this.innerHTML = template;
+    this.$el = $(this);
+  };
+
+  GsHowtoToolbarProto.attachedCallback = function(){
+    this.addEvents();
+  };
+
+  GsHowtoToolbarProto.attributeChangedCallback = function(){
+    console.log('attributeChangedCallback');
+  };
+
+  GsHowtoToolbarProto.detachedCallback = function(){
+    this.removeEvents();
+  };
+
+  GsHowtoToolbarProto.addEvents = function(){
+  };
+
+  GsHowtoToolbarProto.removeEvents = function(){
+  };
+
+
+  document.registerElement('gs-howto-toolbar', {
+    prototype: GsHowtoToolbarProto
+  });
+
+})(document, $);
